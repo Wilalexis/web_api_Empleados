383080eb8514376ef0d9c44a31d8b35e0827b307
Wilmer Martinez <you@example.comWilmer@DESKTOP-I7LKKTC.(none)>
2020-10-21 18:36:38 -0600

    first commit

---
+++ web_empleados/Models/Empleados.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+namespace web_empleados.Models{
+
+    public class Empleados{
+        [Key]
+        public int id_empleado{get;set;}
+        public string codigo{get;set;}
+
+         public string nombres{get;set;}
+
+         public string apellidos{get;set;}
+
+         public string telefono{get;set;}
+
+         public string fecha_nacimiento{get;set;}
+
+    }
+}
